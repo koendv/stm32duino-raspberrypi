fde40dbb343f2a9bf83d36acb73dbe342f14958f
Koen De Vleeschauwer <kdv@kdvelectronics.eu>
2020-03-01 09:51:10 +0100

    1.3.2

--- Arduino_Tools/build.sh
+++ Arduino_Tools/build.sh
@@ -37,6 +37,6 @@ install -D stm32flash $TOOLDIR
 cd $SRCDIR
 
 cd $TOPDIR
-tar cvf STM32Tools-arm-linux-gnuabihf-1.3.2.tar ./STM32Tools
+tar cf STM32Tools-arm-linux-gnuabihf-1.3.2.tar ./STM32Tools
 
 #not truncated
