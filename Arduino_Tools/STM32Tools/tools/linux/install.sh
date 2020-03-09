c35bcd16246f5fb757b804dc5426caed5fd90adf
Koen De Vleeschauwer <kdv@kdvelectronics.eu>
2020-03-09 14:23:22 +0100

    black magic probe

--- Arduino_Tools/STM32Tools/tools/linux/install.sh
+++ Arduino_Tools/STM32Tools/tools/linux/install.sh
@@ -2,7 +2,7 @@
 
 if sudo [ -w /etc/udev/rules.d ]; then
     echo "Copying udev rules..."
-    for RULE in 45-maple.rules 49-stm32_hid_bl.rules 49-stlinkv1.rules 49-stlinkv2.rules 49-stlinkv2-1.rules
+    for RULE in 45-maple.rules 49-stm32_hid_bl.rules 49-stlinkv1.rules 49-stlinkv2.rules 49-stlinkv2-1.rules 99-blackmagic.rules
     do
       sudo cp -v $RULE /etc/udev/rules.d/
       sudo chown root:root /etc/udev/rules.d/$RULE
